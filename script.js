058a82655b98b02936b5d4fe09cae1eb8534329e
nplsam <psammerrick@gmail.com>
2023-07-28 11:45:00 +0100

    basic function that returns chalk colours

--- script.js
+++ script.js
@@ -26,8 +26,30 @@
 // play again?
 
 import { decks } from 'cards';
+import { hearts, spades, clubs, diamonds } from 'cards/build/suits/index.js';
+
+import chalk from 'chalk';
 
 const deck = new decks.StandardDeck;
+deck.shuffleAll();
 
+const card = deck.draw(1)
+console.log(chalk.green(card))
+
+const colourCards = (card) => {
+    const result = card
+    const id = 10
+    if (result === hearts) { 
+        return chalk.red(`Card: Hearts Value: ${id}`);
+    } else if (result === spades) {
+        return chalk.green(`Card: Spades Value: ${id}`);
+    } else if (result === clubs) {
+        return chalk.blue(`Card: Clubs Value: ${id}`);
+    } else if (result === diamonds) {
+        return chalk.magenta(`Card: Diamonds Value: ${id}`);
+    }
+} 
+
+console.log(colourCards(hearts))
 
 
