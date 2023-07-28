30478edf69874acdd1c4bdd2f7189d90bf4ce004
nplsam <psammerrick@gmail.com>
2023-07-28 10:44:49 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+// pseudo
+
+// Shuffle the deck
+
+// deal 2 cards to user
+// deal 2 cards to cpu
+
+// sum of 2 cards for user
+// sum of 2 cards for cpu
+
+// player decision
+// stick with current cards
+// or pull another card
+
+// each time a card is pulled
+// we have to calculate whether its bust or okay
+// if the player goes over 21, they lose and the game is restarted
+
+// var that stores the players hand
+
+// when the dealer reaches 17 they cannot get another card
+
+// compare score
+// win counter
+
+// play again?
+
+import { decks } from 'cards';
+
+const deck = new decks.StandardDeck;
+
+
+
