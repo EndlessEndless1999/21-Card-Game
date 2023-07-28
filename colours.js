ca6751f569a9b37ef065f85441d782519a63cdcf
nplsam <psammerrick@gmail.com>
2023-07-28 12:47:07 +0100

    testing colours functions

--- colours.js
+++ colours.js
@@ -1,13 +1,31 @@
 import { decks } from 'cards';
 import { hearts, spades, clubs, diamonds } from 'cards/build/suits/index.js';
 import chalk from 'chalk';
+import inquirer from 'inquirer';
 
 const deck = new decks.StandardDeck;
 deck.shuffleAll();
 
-const colourCards = (card) => {
-    const result = card
-    const id = 10
+const getCompCard = () => {
+    const choice = deck.draw(1);
+    return choice;
+}
+
+const getUserCard = () => {
+    let userChoice = "";
+    let validInput = false;
+
+    while(!validInput) {
+        userChoice = inquirer.prompt("Pull another card from the deck: ");
+        if (userChoice === "yes" || "y") {
+            validInput = true;
+        }
+    }
+    return deck.draw(1)
+}
+
+const colourCards = (userChoice) => {
+    const result = getUserCard();
     if (result === hearts) { 
         return chalk.red(`Card: Hearts Value: ${id}`);
     } else if (result === spades) {
@@ -18,16 +36,20 @@ const colourCards = (card) => {
         return chalk.magenta(`Card: Diamonds Value: ${id}`);
     }
 } 
+getUserCard()
 
-const card = deck.draw(1)
+// const card = deck.draw(1)
 
-console.log(chalk.green(card))
-console.log(colourCards(hearts))
-console.log(colourCards(clubs))
-console.log(colourCards(diamonds))
-console.log(colourCards(spades))
+// console.log(chalk.green(card))
+// console.log(colourCards(hearts))
+// console.log(colourCards(clubs))
+// console.log(colourCards(diamonds))
+// console.log(colourCards(spades))
 
-// module.exports = colourCards;
+// module.exports = {
+//     colourCards,
+//     getUserCard,
+// }
 
 
 
